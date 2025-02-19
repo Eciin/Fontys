8a15c9a6677e27fce1fee8de2f47d47bc9385b17
Rayane <R.mouahbi@gmail.com>
2025-02-19 18:33:19 +0100

    Organized the code to be more readable

--- main.tf
+++ main.tf
@@ -1,54 +1,5 @@
-terraform {
-  required_providers {
-    vsphere = {
-      source  = "hashicorp/vsphere"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "vsphere" {
-  user           = var.vsphere_user
-  password       = var.vsphere_password
-  vsphere_server = var.vsphere_server
-}
-
-data "vsphere_datacenter" "dc" {
-  name = "Netlab-DC"
-}
- 
-data "vsphere_compute_cluster" "cluster" {
-  name          = "Netlab-Cluster-B"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
- 
-data "vsphere_resource_pool" "pool" {
-  name          = "i416434"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
- 
-data "vsphere_datastore" "datastore" {
-  name          = "NIM01-I3-DB"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
- 
-data "vsphere_network" "network_internet" {
-  name          = "0124_Internet-DHCP-192.168.124.0_24"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
- 
-data "vsphere_virtual_machine" "Server" {
-  name          = "_Templates/All Courses/Templ_Ubuntu-Linux_Server_22.04.4"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_virtual_machine" "Desktop" {
-  name          = "_Templates/All Courses/Templ_Ubuntu-Linux_Desktop_22.04"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
- 
 resource "vsphere_virtual_machine" "Server" {
-  name             = "ubuntu-student"
+  name             = "Monitoring Server"
   folder           = "_Courses/I3-DB01/i416434"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
@@ -91,7 +42,7 @@ resource "vsphere_virtual_machine" "Server" {
 }
 
 resource "vsphere_virtual_machine" "Desktop" {
-  name             = "ubuntu-student"
+  name             = "Client"
   folder           = "_Courses/I3-DB01/i416434"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
