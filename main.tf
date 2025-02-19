5191118d6770dcb7c4a7af53e54ac71fa21fd62a
Rayane <R.mouahbi@gmail.com>
2025-02-19 18:09:38 +0100

    Ensured that environment variables are loaded automatically

--- main.tf
+++ main.tf
@@ -37,12 +37,17 @@ data "vsphere_network" "network_internet" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
  
-data "vsphere_virtual_machine" "template" {
+data "vsphere_virtual_machine" "Server" {
   name          = "_Templates/All Courses/Templ_Ubuntu-Linux_Server_22.04.4"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+data "vsphere_virtual_machine" "Desktop" {
+  name          = "_Templates/All Courses/Templ_Ubuntu-Linux_Desktop_22.04"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
  
-resource "vsphere_virtual_machine" "vm" {
+resource "vsphere_virtual_machine" "Server" {
   name             = "ubuntu-student"
   folder           = "_Courses/I3-DB01/i416434"
   resource_pool_id = data.vsphere_resource_pool.pool.id
@@ -69,12 +74,55 @@ resource "vsphere_virtual_machine" "vm" {
   }
  
   clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
+    template_uuid = data.vsphere_virtual_machine.Server.id
     linked_clone  = false
  
     customize {
       linux_options {
-        host_name = "ubuntu-student"
+        host_name = "Monitoring-Server"
+        domain    = "local"
+      }
+      network_interface {
+        ipv4_address = "" # Use DHCP
+        ipv4_netmask = 0  # Use DHCP
+      }
+    }
+  }
+}
+
+resource "vsphere_virtual_machine" "Desktop" {
+  name             = "ubuntu-student"
+  folder           = "_Courses/I3-DB01/i416434"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+ 
+  num_cpus = 2
+  memory   = 4096
+ 
+  guest_id = "ubuntu64Guest"
+ 
+  wait_for_guest_net_timeout = 0
+  wait_for_guest_ip_timeout  = 0
+ 
+  network_interface {
+    network_id   = data.vsphere_network.network_internet.id
+    adapter_type = "vmxnet3"
+  }
+ 
+  disk {
+    label            = "disk0"
+    size             = 90
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+ 
+  clone {
+    template_uuid = data.vsphere_virtual_machine.Desktop.id
+    linked_clone  = false
+ 
+    customize {
+      linux_options {
+        host_name = "Client"
         domain    = "local"
       }
       network_interface {
